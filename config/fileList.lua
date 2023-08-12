93ea999baee8e91711f3bc5b288615080c3be0ca
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-08-13 10:22:49 +1200

    Copy fileList.lua from landcruisers

---
+++ config/fileList.lua
@@ -0,0 +1,25 @@
+FileList = {
+
+}
+
+local function RGBAtoHex(r, g, b, a, UTF16)
+    local hex = string.format("%02X%02X%02X%02X", r, g, b, a)
+    if UTF16 == true then
+        return L "[HL=" .. towstring(hex) .. L "]"
+    else
+        return "[HL=" .. hex .. "]"
+    end
+end
+function LoadTableFiles(table, path)
+    for folder, item in pairs(table) do
+        if type(item) == "table" then
+            LoadTableFiles(item, path.."/"..folder)
+        else
+            Log("Landcruisers: Loading ".. RGBAtoHex(50, 150, 50, 255, false)..path.."/"..item..".lua")
+            dofile(path .. "/"..item..".lua")
+        end
+    end
+end
+function LoadFiles()
+    LoadTableFiles(FileList, path)
+end
