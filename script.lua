fed10715f7798715fa2d3fa437a3ac3478a8b4b0
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-08-13 13:42:28 +1200

    Refactor script.lua
    Compartmentalize; group up all API events, mod loop, dofiles, etc

--- script.lua
+++ script.lua
@@ -1,51 +1,59 @@
-
---Call debugMagic in script.lua to have to globally wrap everything
+-----------------DOFILES-----------------
 dofile(path .. "/debugMagic.lua")
 dofile("scripts/forts.lua")
 dofile(path .. "/fileList.lua")
 FileList.LoadFiles()
 dofile(path .. "/BetterLog.lua")
---dofile(path .. "/scripts/wheels/collisions.lua")
 
+---------------API EVENTS----------------
 function Load(gameStart)
-    
-    DeviceManager.Load()
-    TerrainManager.Load()
+    LoadMod(gameStart)
 end
-
 function Update(frame)
-    
-    UpdateFunction("UpdateLogging", "Update", frame)
-
-    UpdateFunction("TerrainManager", "Update", frame)
-    UpdateFunction("Collisions", "Update", frame)
-    UpdateFunction("DeviceManager", "Update", frame)
-    
+    ModLoop(frame)
 end
-
 function OnDeviceCreated(teamId, deviceId, saveName, nodeA, nodeB, t, upgradedId)
     DeviceManager.OnDeviceCreated(teamId, deviceId, saveName, nodeA, nodeB, t, upgradedId)
 end
-
 function OnDeviceMoved(teamId, deviceId, temporaryDeviceId, saveName)
     DeviceManager.OnDeviceMoved(teamId, deviceId, temporaryDeviceId, saveName)
 end
-
 function OnDeviceDeleted(teamId, deviceId, saveName, nodeA, nodeB, t)
     DeviceManager.OnDeviceDeleted(teamId, deviceId, saveName, nodeA, nodeB, t)
 end
 function OnDeviceDestroyed(teamId, deviceId, saveName, nodeA, nodeB, t)
     DeviceManager.OnDeviceDestroyed(teamId, deviceId, saveName, nodeA, nodeB, t)
 end
-
-
-
 function OnDeviceTeamUpdated(oldTeamId, newTeamId, deviceId, saveName)
     DeviceManager.OnDeviceTeamUpdated(oldTeamId, newTeamId, deviceId, saveName)
+end
 
+-----------------MOD-------------------
+
+function LoadMod(gameStart)
+    DeviceManager.Load()
+    TerrainManager.Load()
 end
 
 
+function ModLoop(frame)
+    local startUpdateTime
+    if ModDebug.update then
+        startUpdateTime = GetRealTime()
+    end
+    UpdateFunction("UpdateLogging", "Update", frame)
+
+    UpdateFunction("TerrainManager", "Update", frame)
+    UpdateFunction("Collisions", "Update", frame)
+    UpdateFunction("DeviceManager", "Update", frame)
+
+    if ModDebug.update then
+        local endUpdateTime = GetRealTime()
+        local delta = (endUpdateTime - startUpdateTime) * 1000
+        UpdateLogging.Log("Update took " .. string.format("%.2f", delta) .. "ms, " .. string.format("%.1f", delta/(data.updateDelta * 1000) * 100) .. "%")
+    end
+    
+end
 
 
 --Call again to ensure functionality
