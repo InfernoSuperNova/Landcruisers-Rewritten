e69483a42a818f2877bbf14b9f6a708df6e19faf
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-08-15 00:30:12 +1200

    Calculate and store collider

--- classes/terrain.lua
+++ classes/terrain.lua
@@ -2,24 +2,29 @@ BlockMetaTable = {
     blockIndex = 0,
     nodes = {},
     continuousUpdate = false,
+    colliderPos =  Vec3(),
+    colliderRadius = 0,
 }
 
 function Block(blockIndex, continuousUpdate)
     return BlockMetaTable:new(blockIndex, continuousUpdate)
 end
 function BlockMetaTable:new(blockIndex, continuousUpdate)
-
     local o = {}
     setmetatable(o, self)
     self.__index = self
 
-    o.blockId = blockIndex
+    o.blockIndex = blockIndex
     o.continuousUpdate = continuousUpdate
     local nodeCount = GetBlockVertexCount(blockIndex)
+    o.nodes = {}
     for nodeIndex = 0, nodeCount - 1 do
         local nodePos = GetBlockVertexPos(blockIndex, nodeIndex)
         table.insert(o.nodes, nodePos)
     end
+    local collider = MinimumCircularBoundary(o.nodes)
+    o.colliderPos = Vec3(collider.x, collider.y)
+    o.colliderRadius = collider.r
     return o
 end
 
@@ -31,6 +36,9 @@ function BlockMetaTable:Update()
             local nodePos = GetBlockVertexPos(self.blockIndex, nodeIndex)
             table.insert(self.nodes, nodePos)
         end
+        local collider = MinimumCircularBoundary(self.nodes)
+        self.colliderPos = Vec3(collider.x, collider.y)
+        self.colliderRadius = collider.r
     end
 end
 function BlockMetaTable:GetColliderPos()
@@ -42,3 +50,4 @@ end
 function BlockMetaTable:GetNodes()
     return self.nodes
 end
+
