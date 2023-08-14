0ffccdb849797c28e3ee72b272a317e1f27bb128
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-08-15 00:23:07 +1200

    Wheel manager main loop

--- scripts/devices/wheelManager.lua
+++ scripts/devices/wheelManager.lua
@@ -1,5 +1,24 @@
 WheelManager = {}
 
+function WheelManager.Update(frame)
+    --Group wheels by structureId
+    local wheelStructureGroups = {}
+    for _, wheel in ipairs(data.wheels) do
+        if wheelStructureGroups[wheel.structureId] == nil then
+            wheelStructureGroups[wheel.structureId] = {}
+        end
+        table.insert(wheelStructureGroups[wheel.structureId], wheel)
+    end
+    for _, wheelGroup in pairs(wheelStructureGroups) do
+        local wheelCollider = WheelManager.GetWheelCollider(wheelGroup)
+        local collidingBlocks = WheelManager.CheckWheelGroupCollisions(wheelCollider)
+        
+        for _, block in pairs(collidingBlocks) do
+            WheelManager.CheckWheelGroupOnSegmentColliders(wheelGroup, wheelCollider, block)
+        end
+        
+    end
+end
 
 
 
