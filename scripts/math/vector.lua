828baca94badca732c2fc4c72d9fae71d91ba26e
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-08-15 17:23:11 +1200

    Import vector from alex's vanilla vector class expansion

--- scripts/math/vector.lua
+++ scripts/math/vector.lua
@@ -71,3 +71,136 @@ function MinimumWheelCircularBoundary(wheels)
     end
     return MinimumCircularBoundary(points)
 end
+
+function TableToVector3(table)
+    local newTable = {}
+    for _, value in pairs(table) do
+        table.insert(newTable, Vec3(value.x, value.y))
+    end
+    return newTable
+end
+
+
+
+
+--[[ vector.lua
+Modifies Vec3 to have expanded functionality
+Expects forts.lua or core.lua to be imported beforehand
+]]
+
+-- Vector member functions
+local VectorMembers = {
+    length = function (vec)
+        return Vec3Length(vec)
+    end,
+    radians = function(vec)
+        return math.atan2(vec.y,vec.x)
+    end,
+    degrees = function(vec)
+        return math.deg(math.atan2(vec.y,vec.x))
+    end,
+    distance = function(vec1, vec2)
+        return (vec1 - vec2).length()
+    end,
+
+    -- Modifies the existing vector
+    normalize = function(vec)
+        local vecLength = vec.length()
+        if (vecLength == 0) then return end 
+
+        vec.x = vec.x / vecLength
+        vec.y = vec.y / vecLength
+        vec.z = vec.z / vecLength
+    end,
+}
+
+
+
+--Vanilla Vec3 expansion, made by AlexD
+
+
+
+-- Metatable implementing operators using metafunctions
+-- All member functions are implemented via the __index metafunction
+local VectorMetatable = {
+    __add = function(value1, value2)
+        if type(value1) == "table" and type(value2) == "table" then
+            return Vec3(value1.x+value2.x, value1.y+value2.y, value1.z+value2.z)
+        end
+        error("Error: Addition cannot be calculated on type " .. type(value1) .. " and type " .. type(value2) .. "",2)
+    end,
+    __sub = function(value1, value2)
+        if type(value1) == "table" and type(value2) == "table" then
+            return Vec3(value1.x-value2.x, value1.y-value2.y, value1.z-value2.z)
+        end
+        error("Error: Subtraction cannot be calculated on type " .. type(value1) .. " and type " .. type(value2) .. "",2)
+    end,
+    __mul = function(value1, value2)
+        local type1 = type(value1)
+        local type2 = type(value2)
+        if type1 == "number" then 
+            return Vec3(value2.x*value1, value2.y*value1, value2.z*value1)
+        elseif type2 == "number" then 
+            return Vec3(value1.x*value2, value1.y*value2, value1.z*value2)
+        elseif type1 ~= "table" or type2 ~= "table" then
+            error("Error: Multiplication cannot be calculated on type " .. type1 .. " and type " .. type2 .. "", 2)
+        end
+        
+        return Vec3(value1.x*value2.x, value1.y*value2.y, value1.z*value2.z)
+    end,
+    __div = function(value1, value2)
+        local type1 = type(value1)
+        local type2 = type(value2)
+        if type1 == "number" then 
+            return Vec3(value1/value2.x, value1/value2.y, value1/value2.z)
+        elseif type2 == "number" then 
+            return Vec3(value1.x/value2, value1.y/value2, value1.z/value2)
+        elseif type1 ~= "table" or type2 ~= "table" then
+            error("Error: Division cannot be calculated on type " .. type1 .. " and type " .. type2 .. "", 2)
+        end
+        
+        return Vec3(value1.x/value2.x, value1.y/value2.y, value1.z/value2.z)
+    end,
+    __unm = function(vector3)
+        return Vec3(-vector3.x, -vector3.y, -vector3.z)
+    end,
+    __tostring = function(vector3)
+        return string.format("(%.2f, %.2f, %.2f)", vector3.x, vector3.y, vector3.z)
+    end,
+    __concat = function(value1, value2)
+        return tostring(value1) .. tostring(value2)
+    end,
+    __eq = function(vec1, vec2)
+        return vec1.x == vec2.x and vec1.y == vec2.y and vec1.z == vec2.z
+    end,
+
+    -- Implements member functions, called when a nil item *would*
+    -- be accessed in the tables dictionary, instead gives return value. 
+    __index = function(vec, index)
+        local member = VectorMembers[index]
+        if (member ~= nil) then
+            -- Wraps the member function to avoid the need to explicitly pass in the vector.
+            local memberWrapper = function(...)
+                return member(vec, ...)
+            end
+            return memberWrapper
+        end
+    end,
+}
+
+-- Vector constructor
+function Vec3(x, y, z)
+    local vec = {}
+    vec.x = x or 0
+    vec.y = y or 0
+    vec.z = z or 0
+    setmetatable(vec, VectorMetatable)
+    return vec
+end
+
+-- Sets properties to be that of a Vector.
+function Vec3Restore(vec)
+    if (vec) then
+        setmetatable(vec, VectorMetatable)
+    end
+end
