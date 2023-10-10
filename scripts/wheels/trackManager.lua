692176566ca25787a6ffe5d1d3eddc22989d0024
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-10-10 15:38:21 +1300

    Trackmanager functionality

--- scripts/wheels/trackManager.lua
+++ scripts/wheels/trackManager.lua
@@ -1,9 +1,16 @@
-TrackManager = {}
+TrackManager = {
+    trackSets = {}
+}
 function TrackManager.Update()
-    local wheelStructures = TrackManager.IndexWheels()
-    for _, wheels in pairs(wheelStructures) do
-        TrackSet(wheels)
+    for _, trackSet in pairs(TrackManager.trackSets) do
+        trackSet:Update()
     end
+end
+
+function TrackManager.Load()
+    TrackManager.IndexAtLoad()
+end
+
 function TrackManager.AddWheel(wheel)
     
     local structureId = wheel.structureId
@@ -16,10 +23,21 @@ function TrackManager.AddWheel(wheel)
     end
     TrackManager.trackSets[structureId] = trackSet
 end
-    
+
+function TrackManager.RemoveWheel(wheel)
+    local structureId = wheel.structureId
+    local trackSet = TrackManager.trackSets[structureId]
+    if not trackSet then
+        return
+    else
+        trackSet:RemoveWheel(wheel)
+        if #trackSet.wheels == 0 then
+            TrackManager.trackSets[structureId] = nil
+        end
+    end
 end
 
-function TrackManager.IndexWheels()
+function TrackManager.IndexAtLoad()
     --Group wheels based on structure id
     local wheelStructures = {}
     for _, wheel in pairs(data.wheels) do
@@ -28,5 +46,11 @@ function TrackManager.IndexWheels()
         end
         table.insert(wheelStructures[wheel.structureId], wheel)
     end
-    return wheelStructures
+    for structure, wheels in pairs(wheelStructures) do
+        local trackSet = TrackSet(wheels)
+        if not TrackManager.trackSets[structure] then
+            TrackManager.trackSets[structure] = {}
+        end
+        TrackManager.trackSets[structure] = trackSet
+    end
 end
