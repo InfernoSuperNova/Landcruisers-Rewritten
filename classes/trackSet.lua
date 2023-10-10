68fa34a9831abb370d87bc59f50a25da31accdb8
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-10-10 15:36:54 +1300

    TrackSet functionality

--- classes/trackSet.lua
+++ classes/trackSet.lua
@@ -1,5 +1,8 @@
 TrackSetMetaTable = {
-    straights = {}
+    prevPosition = 0, --For interpolation
+    position = 0,
+    wheels = {},
+    track = {},
 }
 
 function TrackSet(wheels)
@@ -10,18 +13,40 @@ function TrackSetMetaTable:new(wheels)
     local o = {}
     setmetatable(o, self)
     self.__index = self
+    o.wheels = wheels
+    o.track = {}
+    return o
+end
+
+function TrackSetMetaTable:Update()
+    self.track = {}
     local circles = {}
-    for _, wheel in pairs(wheels) do
+    for _, wheel in pairs(self.wheels) do
         local pos = wheel:GetDisplacedPos()
         local radius = wheel.type:GetRadius()
         table.insert(circles, {pos = pos, radius = radius})
     end
     local hull = Tracks.GiftWrapping(circles)
-    o.straights = Tracks.GetStraights(hull)
+    local straightSegments = Tracks.GetStraights(hull)
+    for i = 1, #self.wheels do
+        table.insert(self.track, self.wheels[i])
+        table.insert(self.track, straightSegments[i])
+    end
+end
+
+function TrackSetMetaTable:AddWheel(wheel)
+    table.insert(self.wheels, wheel)
 end
 
 
-
+function TrackSetMetaTable:RemoveWheel(wheel)
+    for i = 1, #self.wheels do
+        if self.wheels[i] == wheel then
+            table.remove(self.wheels, i)
+            return
+        end
+    end
+end
 
 TrackStraightMetaTable = {
     a = Vec3(),
