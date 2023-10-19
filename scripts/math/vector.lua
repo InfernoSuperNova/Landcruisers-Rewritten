f69736f7633e3ecf7f166e160e19fcebb3931852
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-10-19 14:46:23 +1300

    Vector3 linear interpolation

--- scripts/math/vector.lua
+++ scripts/math/vector.lua
@@ -204,3 +204,11 @@ function Vec3Restore(vec)
         setmetatable(vec, VectorMetatable)
     end
 end
+
+function Vec3Lerp(vec1, vec2, t)
+    return Vec3(
+        vec1.x + (vec2.x - vec1.x) * t,
+        vec1.y + (vec2.y - vec1.y) * t,
+        vec1.z + (vec2.z - vec1.z) * t
+    )
+end
