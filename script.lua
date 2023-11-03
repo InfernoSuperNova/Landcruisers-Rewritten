7441ffa1ba4425de7686979592e0cfc0cd278baf
Flinnshadow <117613189+Flinnshadow@users.noreply.github.com>
2023-11-03 05:12:41 -0500

    Small changes, remove prob extra DeepCopy statment

--- script.lua
+++ script.lua
@@ -3,9 +3,10 @@
 -----------------DOFILES-----------------
 dofile(path .. "/debugMagic.lua")
 dofile("scripts/forts.lua")
-dofile("scripts/core.lua")
-dofile(path .. "/fileList.lua")
+--dofile("scripts/core.lua")
+--dofile("scripts/core_utility.lua") -- Doesn't seem to be required, only for deepcopy 
 dofile(path .. "/BetterLog.lua")
+dofile(path .. "/fileList.lua")
 FileList.LoadFiles()
 
 ---------------API EVENTS----------------
@@ -99,25 +100,5 @@ function ModDraw()
         PreviousDrawTime = newDrawTime
         Graph.Update()
 end
-
-
-
-
-
-function DeepCopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[DeepCopy(orig_key)] = DeepCopy(orig_value)
-        end
-        setmetatable(copy, DeepCopy(getmetatable(orig)))
-    else -- number, string, boolean, etc
-        copy = orig
-    end
-    return copy
-end
-
 
 dofile(path .. "/debugMagic.lua")
