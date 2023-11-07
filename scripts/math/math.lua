89f911c780f5caf37a5a0a39bb88cf2bcdd803ae
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-11-08 01:22:03 +1300

    math.clamp function

--- scripts/math/math.lua
+++ scripts/math/math.lua
@@ -4,5 +4,9 @@ function math.sign(x)
     return x > 0 and 1 or x < 0 and -1 or 0
 end
 
+function math.clamp(x, min, max)
+    return math.min(math.max(x, min), max)
+end
+
 DegToRad = math.pi / 180
 RadToDeg = 180 / math.pi
