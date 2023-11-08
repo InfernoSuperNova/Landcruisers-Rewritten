eede1f01bdef911aff6c69c025576b9e3b19d0f3
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-11-08 18:19:02 +1300

    math.lerp

--- scripts/math/math.lua
+++ scripts/math/math.lua
@@ -8,5 +8,9 @@ function math.clamp(x, min, max)
     return math.min(math.max(x, min), max)
 end
 
+function math.lerp(a, b, t)
+    return a + (b - a) * t
+end
+
 DegToRad = math.pi / 180
 RadToDeg = 180 / math.pi
