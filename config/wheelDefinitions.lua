2e85444bb0ff7b3f2e527f9b194897c8ba550dc2
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-12-04 22:18:56 +1300

    Meta modding support for wheel parameters

--- config/wheelDefinitions.lua
+++ config/wheelDefinitions.lua
@@ -47,7 +47,7 @@ WheelDefinitionHelpers = {}
 
 function WheelDefinitionHelpers.GetWheelDefinitionBySaveName(saveName)
     for _, wheelDefinition in ipairs(WheelDefinitions) do
-        local definitionSaveName = wheelDefinition:GetSaveName()
+        local definitionSaveName = wheelDefinition.saveName
         if saveName == definitionSaveName then
            return wheelDefinition
         elseif saveName == definitionSaveName .. WheelConfig.invertedNameTag then
@@ -61,6 +61,9 @@ function WheelDefinitionHelpers.GetWheelDefinitionBySaveName(saveName)
 end
 
 function WheelDefinitionHelpers.ConstructWheelDefinitions()
+    BetterLogToFile("///////////////////////////////////////////////////////////////////////////////////////////////////////")
+    BetterLogToFile("///////////////////////////////////LANDCRUISERS WHEEL DEFINITIONS//////////////////////////////////////")
+    BetterLogToFile("///////////////////////////////////////////////////////////////////////////////////////////////////////")
     local newDefinitions = {}
     for _, wheelDefinition in ipairs(WheelDefinitions) do
         --check if the last character of the saveName is the inverted tag
@@ -70,4 +73,56 @@ function WheelDefinitionHelpers.ConstructWheelDefinitions()
         if wheelDefinition.radius > LargestWheelRadius then LargestWheelRadius = wheelDefinition.radius end --set largest wheel radius
     end
     WheelDefinitions = newDefinitions
+    newDefinitions = WheelDefinitionHelpers.ConstructMetaWheelDefinitions(newDefinitions)
+    WheelDefinitions = newDefinitions
+    LogToFile("Final wheel definitions:")
+    BetterLogToFile(WheelDefinitions)
+    BetterLogToFile("///////////////////////////////////////////////////////////////////////////////////////////////////////")
+    BetterLogToFile("/////////////////////////////////END LANDCRUISERS WHEEL DEFINITIONS////////////////////////////////////")
+    BetterLogToFile("///////////////////////////////////////////////////////////////////////////////////////////////////////")
 end
+
+--meta modding support
+function WheelDefinitionHelpers.ConstructMetaWheelDefinitions(newDefinitions)
+    local projectileTypeCount = GetProjectileTypeCount(1)
+
+    for i = 1, projectileTypeCount do
+        local metaWheelDefinitionsString = GetProjectileParamStringByIndex(i, 0, MetaModdingParamName, NoParamName)
+
+        if metaWheelDefinitionsString ~= NoParamName then
+            local metaWheelDefinitions = Json.decode(metaWheelDefinitionsString)
+            for _, metaWheelDefinition in ipairs(metaWheelDefinitions) do
+                --check if a wheel definition with the same saveName already exists
+                local currentDefinition = WheelDefinitionHelpers.GetWheelDefinitionBySaveName(metaWheelDefinition.saveName)
+                
+                if currentDefinition ~= nil then
+                    LogToFile("Updating wheel definition: " .. metaWheelDefinition.saveName)
+                    WheelDefinitionHelpers.UpdateWheelDefinition(currentDefinition, metaWheelDefinition)
+                else
+                    LogToFile("Creating new wheel definition: " .. metaWheelDefinition.saveName)
+                    for key, value in pairs(metaWheelDefinition) do
+                        if key:sub(1,1) ~= "_" then
+                            LogToFile("    With value " .. key .. ": " .. tostring(value))
+                        end
+                        
+                    end
+                    local wheel = WheelDefinition:new(metaWheelDefinition.radius, metaWheelDefinition.height, metaWheelDefinition.dampening, metaWheelDefinition.spring, metaWheelDefinition.traction,
+                    metaWheelDefinition.bearingEnergyLoss, metaWheelDefinition.mass, metaWheelDefinition.saveName, metaWheelDefinition.sprocketSprite, metaWheelDefinition.wheelSprite, false)
+                    table.insert(newDefinitions, wheel)
+                    if metaWheelDefinition.radius > LargestWheelRadius then LargestWheelRadius = metaWheelDefinition.radius end --set largest wheel radius
+                end
+            end
+        end
+    end
+    return newDefinitions
+end
+
+function WheelDefinitionHelpers.UpdateWheelDefinition(currentDefinition, newDefinition)
+    for key, value in pairs(newDefinition) do 
+        if key ~= "saveName" and key:sub(1,1) ~= "_" then
+            LogToFile("    Updated " .. key .. ": was " .. tostring(currentDefinition[key]) .. ", now " .. tostring(value))
+            currentDefinition[key] = value
+        end
+    end
+end
+
