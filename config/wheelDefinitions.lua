85db1ab8ba3bbd47f6918bc9def9fdfa50378822
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-12-04 22:18:28 +1300

    Meta modding parameter names

--- config/wheelDefinitions.lua
+++ config/wheelDefinitions.lua
@@ -39,6 +39,9 @@ WheelDefinitions = {
 }
 LargestWheelRadius = 0
 DefaultWheelDefinition = WheelDefinition:new(75, 0, 30000, 3000, 100, 100, 100, "smallSuspension", "/effects/track_sprocket.lua", "/effects/wheel.lua")
+
+MetaModdingParamName = "LCWheelStats"
+NoParamName = "noParams"
 WheelDefinitionHelpers = {}
 
 
