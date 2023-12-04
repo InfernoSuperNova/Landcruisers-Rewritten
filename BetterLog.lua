2e80f0aaea85d183ffd4c3891bd0de6a603075c8
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-12-04 22:19:08 +1300

    BetterLogToFile functionality

--- BetterLog.lua
+++ BetterLog.lua
@@ -9,50 +9,59 @@
 --Table : table to log
 --IndentLevel : indentation level of the table content (ex : 1 if it's the first time the function is called)
 
-function LogTables(Table, IndentLevel)
+function LogTables(Table, IndentLevel, ToFile)
 
     if Table == nil then
-        Log("nil")
+        LogOption("nil", ToFile)
     else
         IndentLevel = IndentLevel or 1
         local indent = string.rep("    ", IndentLevel)
         local indentinf = string.rep("    ", IndentLevel-1)
         local metatable = getmetatable(Table)
         if metatable and metatable.__tostring then
-            Log(indent .. tostring(Table) .. ",")
+            LogOption(indent .. tostring(Table) .. ",", ToFile)
         else
-            Log(indentinf .. "{")
+            LogOption(indentinf .. "{", ToFile)
             for k, v in pairs(Table) do
                 if type(k) == "number" then
                     if type(v) == "table" then
-                        Log(indent .. "[" .. tostring(k) .. "] = ")
-                        LogTables(v, IndentLevel + 1)
+                        LogOption(indent .. "[" .. tostring(k) .. "] = ", ToFile)
+                        LogTables(v, IndentLevel + 1, ToFile)
                     elseif type(v) == "function" then
-                        LogFunction(v, indent)
+                        LogFunction(v, indent, ToFile)
                     elseif type(v) == "string" then
-                        Log(indent .. "[" .. tostring(k) .. '] = "' .. v .. '",')
+                        LogOption(indent .. "[" .. tostring(k) .. '] = "' .. v .. '",', ToFile)
                     else
-                        Log(indent .. "[" .. tostring(k) .. "] = " .. tostring(v) .. ",")
+                        LogOption(indent .. "[" .. tostring(k) .. "] = " .. tostring(v) .. ",", ToFile)
                     end
                 else
                     if type(v) == "table" then
-                        Log(indent .. tostring(k) .. " = ")
-                        LogTables(v, IndentLevel + 1)
+                        LogOption(indent .. tostring(k) .. " = ", ToFile)
+                        LogTables(v, IndentLevel + 1, ToFile)
                     elseif type(v) == "function" then
-                        LogFunction(v, indent)
+                        LogFunction(v, indent, ToFile)
                     elseif type(v) == "string" then
-                        Log(indent .. tostring(k) .. ' = "' .. v .. '",')
+                        LogOption(indent .. tostring(k) .. ' = "' .. v .. '",', ToFile)
                     else
-                        Log(indent .. tostring(k) .. " = " .. tostring(v) .. ",")
+                        LogOption(indent .. tostring(k) .. " = " .. tostring(v) .. ",", ToFile)
                     end
                 end
             end
             if IndentLevel > 1 then
-                Log(indentinf .. "},")
+                LogOption(indentinf .. "},", ToFile)
             else
-                Log(indentinf .. "}")
+                LogOption(indentinf .. "}", ToFile)
             end
         end
+    end
+end
+
+
+function LogOption(str, ToFile)
+    if ToFile then
+        LogToFile(str)
+    else
+        Log(str)
     end
 end
 
@@ -60,11 +69,11 @@ end
 --Function LogFunction
 --If FindFunctionName is present, logs the name of the function (instead of the memory adress)
 
-function LogFunction(Func, indent)
+function LogFunction(Func, indent, ToFile)
     if FindFunctionName and FindFunctionName(Func) then
-        Log(indent .. "function : " .. FindFunctionName(Func))
+        LogOption(indent .. "function : " .. FindFunctionName(Func), ToFile)
     else
-        Log(tostring(Func))
+        LogOption(tostring(Func), ToFile)
     end
 end
 
@@ -79,11 +88,27 @@ function BetterLog(v)
         if metatable and metatable.__tostring then --if the table has a built in print method, use it
             Log(tostring(v))
         else
-            LogTables(v) --otherwise use the default method of tables
+            LogTables(v, 1, false) --otherwise use the default method of tables
         end
     elseif type(v) == "function" then
-        LogFunction(v, "")
+        LogFunction(v, "", false)
     else
         Log(tostring(v))
+    end
+end
+
+
+function BetterLogToFile(v)
+    if type(v) == "table" then
+        local metatable = getmetatable(v) --metatables are a lua feature to modify how table behave (mainly operators). Vec3 has one allowing you to use + and * on them like a mathematical vector
+        if metatable and metatable.__tostring then --if the table has a built in print method, use it
+            LogToFile(tostring(v))
+        else
+            LogTables(v, 1, true) --otherwise use the default method of tables
+        end
+    elseif type(v) == "function" then
+        LogFunction(v, "", true)
+    else
+        LogToFile(tostring(v))
     end
 end
